e05b638ac19819d6ba0402d9082829a40dca0826
Md Tauseef Akhtar <mohammadtauseef284@gmail.com>
2024-01-31 17:16:44 +0530

    Testing

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -28,7 +28,7 @@ const SigninForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/logo.png" alt="logo" />
+        <img src="/assets/images/logon.png" alt="logo" height={50} width={75} />
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Log in to your account</h2>
         <p className="text-light-3 small-medium md:base-regular">
           Welcome Back! Please enter your details
