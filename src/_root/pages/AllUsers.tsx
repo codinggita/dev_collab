532da03fe517a4998c7ac161fcb82d26ab5e2445
Md Tauseef Akhtar <mohammadtauseef284@gmail.com>
2024-02-02 15:09:06 +0530

    Changes

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -1,7 +1,19 @@
 const AllUsers = () => {
-  return (
-    <div>AllUsers</div>
-  )
-}
 
-export default AllUsers
+  return (
+    <div className="saved-container">
+      <div className="flex gap-2 w-full max-w-5xl">
+        <img
+          src="/assets/icons/save.svg"
+          width={36}
+          height={36}
+          alt="edit"
+          className="invert-white"
+        />
+        <h2 className="h3-bold md:h2-bold text-left w-full">Collaborate With Users</h2>
+      </div>
+    </div>
+  );
+};
+
+export default AllUsers;
